4d36e696c44a8e2a3264bfec2353df0b58aae829
Harikrishnan <harivk1998@gmail.com>
2024-01-18 17:33:03 +0530

    newchanges

--- backend/auth/verifyDoctorToken.js
+++ backend/auth/verifyDoctorToken.js
@@ -34,8 +34,10 @@ export const restrict = (role) => async (req, res, next) => {
   const userId = req.userId;
   let user;
 
+
   const patient = await User.findById(userId);
   const doctor = await Doctor.findById(userId);
+  console.log(doctor)
 
   if (patient) {
     user = patient;
