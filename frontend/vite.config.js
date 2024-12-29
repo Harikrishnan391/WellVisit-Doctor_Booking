4692959ace116d4fa8045db4fe53a8edc62d9c54
Harikrishnan391 <harivk1998@example.com>
2024-12-30 01:27:53 +0530

    add base  in viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/',
   build: {
-    outDir: "dist", // Set the output directory to 'build' as Render expects
+    outDir: "dist",
   },
+  plugins: [react()],
 });
